e833dadf64d051fd7d617ee91f590ada834e1fd6
odgerel <o_d_g_e_r_e_l@yahoo.com>
2021-12-18 08:39:38 +0800

    Update wifi_hack_pass.bat

--- wifi_hack_pass.bat
+++ wifi_hack_pass.bat
@@ -1,16 +1,5 @@
 @ECHO OFF 
 
-::netsh wlan show profiles > wlan_list.txt
-::myVar=$(myCmd)
-
-::for /f "delims=" %%A in ('netsh wlan show profiles') do set volume=%%A
-
-
-::echo %volume%
-
-::FOR /f "tokens=2 delims=:" %%i in ('netsh wlan show profiles') DO set var=%%i
-::echo %var%
-
 setlocal enableDelayedExpansion
 set /a c=0
 for /F delims^=^ eol^= %%r in ('netsh wlan show profiles') DO (
@@ -18,31 +7,17 @@ for /F delims^=^ eol^= %%r in ('netsh wlan show profiles') DO (
 	set /A c+=1
 		If !c! GEQ 7 ( 
 			FOR /f "tokens=2 delims=:" %%i in ("%%r") DO (
-			for /f "tokens=* delims= " %%a in ("%%i") do echo netsh wlan show profiles "%%a" key=clear
+			for /f "tokens=* delims= " %%a in ("%%i") do (
+				set /a c1=0
+				for /F delims^=^ eol^= %%s in ('netsh wlan show profiles "%%a" key"="clear') DO (
+					set /A c1+=1
+					If !c1! == 28 (
+						FOR /f "tokens=2 delims=:" %%m in ("%%s") DO for /f "tokens=* delims= " %%p in ("%%m") do echo %%a : %%p
+						)
+					)
+				
+				)
 			)
 		)
 	)
 for /f "tokens=* delims= " %%a in ("%str%") do set str=%%a
-
-::FOR /f "tokens=2 delims=:" %%i in ("%%r") DO echo %%i name
-
-REM set var=ipconfig
-REM For /f %%a in ('%var%') DO echo %%a :: хувьсагч аван коммандыг ажиллуан гарсан зр дүнгээс хайот хийнэ.
-
-REM set var=ipconfig
-REM For /f %%a in ("%var%") DO echo %%a :: хувьсагч аван хайлт хийнэ
-
-::echo !line[10]!
-
-::FOR /L %%i IN (7 1 2) DO  (
-::   call echo Name = %%obj[%%i].Name%%
-::   call echo Value = %%obj[%%i].ID%%
-::)
-
-
-REM for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
-REM if "%version%" == "6.0" echo Windows Vista.
-REM if "%version%" == "6.1" echo Windows 7
-REM if "%version%" == "6.2" echo Windows 8
-REM if "%version%" == "6.3" echo Windows 8.1
-REM if "%version%" == "10.0" echo Windows 10.
